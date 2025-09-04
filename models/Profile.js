9fa6cf9eb1800c999ad9ce5061c56088c8eccdd0
Akshat-66 <akshatripathi.1@gmail.com>
2025-09-04 21:31:56 +0530

    schema defining

---
+++ models/Profile.js
@@ -0,0 +1,44 @@
+// backend/models/Profile.js
+const mongoose = require('mongoose');
+
+const profileSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    education: [
+        { type: String, }
+    ],
+    skills: [
+        {
+            type: String,
+        }
+    ],
+    projects: [
+        {
+            title: { type: String, required: true },
+            description: { type: String },
+            links: { type: String }
+        }
+    ],
+    work: [
+        {
+            title: { type: String, required: true },
+            description: { type: String },
+        }
+    ],
+    links: {
+        github: { type: String },
+        linkedin: { type: String },
+        portfolio: { type: String }
+    }
+}, { timestamps: true });
+
+
+const Profile = mongoose.model('Profile', profileSchema);
+module.exports = Profile;
--- models/user.js
+++ models/user.js
@@ -1,2 +1,9 @@
 const mongoose = require('mongoose');
 
+const Userschema = new mongoose.Schema({
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+});
+
+const User = mongoose.model('User',Userschema);
+module.exports = User;
